8f28db4ae313082f01d0405adb8a636597032c48
Ryan Jarnutowski <rjarnutowski@bluestatedigital.com>
2018-12-17 13:58:06 -0500

    [CHEF-194] fixing gems

--- recipes/bastion.rb
+++ recipes/bastion.rb
@@ -22,12 +22,21 @@ include_recipe "rvm::system_install"
 perform_install_rubies  = node['rvm']['install_rubies'] == true ||
                   node['rvm']['install_rubies'] == "true"
 
+node['rvm']['gems'] = {
+    'ruby-2.5.3' =>[
+        {'name' => 'knife-acl'},
+        {'name' => 'knife-supermarket'}
+    ]
+}
+
+
 if perform_install_rubies
-  install_rubies  :rubies => node['rvm']['knife_rubies'],
+  install_rubies  :rubies => node['rvm']['rubies'],
                   :default_ruby => node['rvm']['knife_ruby'],
                   :global_gems => node['rvm']['global_gems'],
                   :gems => node['rvm']['gems']
 end
+
 
 # add users to rvm group
 group 'rvm' do
